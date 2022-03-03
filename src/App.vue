2e6d0c7997fafd526c94e3ef368b953bad74ebc8
ismailbentabett <ismailbentabett@gmail.com>
2022-03-03 22:16:13 +0100

    client 1.0.0 : :fire: fixed is guest isseus oath  :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class=" h-screen min-h-full flex flex-col justify-between">
-    <Navbar v-if="path && isLoggedin == false"  :authUser="authUser"/>
+  <div class="h-screen min-h-full flex flex-col justify-between">
+    <Navbar v-if="path && isLoggedin == false" :authUser="authUser" />
     <AuthNavbar v-if="path && isLoggedin == true" :authUser="authUser" />
     <router-view />
     <Footer />
   </div>
 </template>
 <script>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { computed, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 import Footer from './components/Footer.component.vue';
 import Navbar from './components/Navbar/Navbar.component.vue';
@@ -18,9 +18,24 @@ export default {
   name: 'App',
   components: { Footer, Navbar, AuthNavbar },
   setup() {
+    const router = useRouter();
     const route = useRoute();
     const path = computed(() => route.path !== '/');
     const store = useStore();
+
+    onMounted( async () => {
+
+        const authUser = await store.dispatch("auth/getAuthUser");
+        if (authUser) {
+          store.dispatch("auth/setGuest", { value: "isNotGuest" });
+          router.push("/dashboard");
+        } else {
+          const error = Error(
+            "Unable to fetch user after login, check your API settings."
+          );
+          error.name = "Fetch User";
+          throw error;
+        } }     );
 
   const authUser = computed(() => store.getters["auth/authUser"]);
     const isLoggedin = computed(() => store.getters['auth/loggedIn']);
@@ -30,5 +45,4 @@ export default {
 </script>
 
 <style>
-
 </style>
