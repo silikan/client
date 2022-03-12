d433cc7c1507d15ab099e718006ef38916aafe64
ismailbentabett <ismailbentabett@gmail.com>
2022-03-12 10:53:20 +0100

    client 1.0.0 : added button and route logic to handymen

--- src/components/Handymen_List.component.vue
+++ src/components/Handymen_List.component.vue
@@ -6,7 +6,7 @@
           class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none"
         >
           <h2 class="text-3xl font-extrabold tracking-tight hidden md:block">
-            Our Team
+            Handymen
           </h2>
         </div>
         <div class="flex items-center flex-1 md:flex-none">
@@ -26,6 +26,9 @@
           <li v-for="person in people" :key="person.name">
             <div class="space-y-4">
               <div class="aspect-w-3 aspect-h-2">
+               <router-link :to="`/user/${person.id}`">
+
+
                 <img
                   class="md:h-60 md:w-96 object-cover shadow-lg rounded-lg h-69"
                   :src="person.avatar_svg"
@@ -38,55 +41,83 @@
                   v-if="person.avatarWithoutLocalhost !== null"
                   alt=""
                 />
+                 </router-link>
               </div>
-
-              <div class="space-y-2">
-                <div class="text-lg leading-6 font-medium space-y-1">
-                  <h3>{{ person.name }}</h3>
-                  <p class="text-indigo-600">
-                    {{ person.role }}
-                  </p>
+              <div class="flex justify-between">
+                <div class="space-y-2">
+                  <div class="text-xl leading-6 font-medium space-y-1">
+                    <h3>{{ person.name }}</h3>
+                    <p class="text-sm font-bold text-indigo-600">
+                      @{{ person.username }}
+                    </p>
+                  </div>
+                  <ul role="list" class="flex space-x-5">
+                    <li>
+                      <a
+                        :href="person.twitterUrl"
+                        class="text-gray-400 hover:text-gray-500"
+                      >
+                        <span class="sr-only">Twitter</span>
+                        <svg
+                          class="w-5 h-5"
+                          aria-hidden="true"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        :href="person.linkedinUrl"
+                        class="text-gray-400 hover:text-gray-500"
+                      >
+                        <span class="sr-only">LinkedIn</span>
+                        <svg
+                          class="w-5 h-5"
+                          aria-hidden="true"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-                <ul role="list" class="flex space-x-5">
-                  <li>
-                    <a
-                      :href="person.twitterUrl"
-                      class="text-gray-400 hover:text-gray-500"
-                    >
-                      <span class="sr-only">Twitter</span>
-                      <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"
-                        />
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      :href="person.linkedinUrl"
-                      class="text-gray-400 hover:text-gray-500"
-                    >
-                      <span class="sr-only">LinkedIn</span>
-                      <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </a>
-                  </li>
-                </ul>
+                <div class="self-end">
+                 <router-link :to="`/user/${person.id}`">
+                  <button
+                    class="
+                      inline-flex
+                      items-center
+                      px-12
+                      py-1
+                      border border-transparent
+                      text-sm
+                      font-medium
+                      rounded-full
+                      shadow-sm
+                      text-white
+                      bg-indigo-600
+                      hover:bg-indigo-700
+                      focus:outline-none
+                      focus:ring-2
+                      focus:ring-offset-2
+                      focus:ring-indigo-500
+                    "
+                  >
+                    Hire
+                  </button>
+                  </router-link>
+                </div>
               </div>
             </div>
           </li>
@@ -176,8 +207,7 @@
             >
               <a
                 class="
-                                cursor-pointer
-
+                  cursor-pointer
                   relative
                   inline-flex
                   items-center
@@ -193,8 +223,7 @@
                   max-w-md
                 "
                 @click="prevPage"
-                            v-if="links.prev"
-
+                v-if="links.prev"
               >
                 <span class="sr-only">Previous</span>
                 <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
@@ -257,7 +286,7 @@
               </a>
 
               <a
-              v-if="totalPages > 1"
+                v-if="totalPages > 1"
                 class="
                   cursor-pointer
                   border-gray-300
@@ -285,7 +314,7 @@
               </a>
               <a
                 class="
-                cursor-pointer
+                  cursor-pointer
                   relative
                   inline-flex
                   items-center
@@ -300,8 +329,7 @@
                   hover:bg-gray-50
                 "
                 @click="nextPage"
-                            v-if="links.next"
-
+                v-if="links.next"
               >
                 <span class="sr-only">Next</span>
                 <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
@@ -387,7 +415,7 @@ export default {
 
     let users = computed(() => store.getters["handyman/Handymen"]);
 
-console.log(users.value);
+    console.log(users.value);
 
     let people = computed(() => {
       let data = reactive([]);
@@ -420,7 +448,7 @@ console.log(users.value);
           avatar: avatar,
           avatarWithoutLocalhost: avatarWithoutLocalhost,
           email: user.email,
-          role: user.role,
+          username: user.username,
         });
       });
       return data;
@@ -465,7 +493,7 @@ console.log(users.value);
 
       return data;
     });
-    console.log(filterPages.value);
+    console.log(people.value);
     return {
       people,
 
