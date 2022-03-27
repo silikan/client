b8521ef8d13b68558d46eedcb69e03ddcc471af8
ismailbentabett <ismailbentabett@gmail.com>
2022-03-27 16:42:19 +0100

    client 1.0.0 : setup room ui

--- src/components/Chat/ChatRoom.component.vue
+++ src/components/Chat/ChatRoom.component.vue
@@ -1,14 +1,124 @@
 <template>
-{{to}}
-{{from}}
   <div class="flex justify-center p-10">
     <div class="w-full border rounded">
       <div class="">
         <div class="w-full h-screen flex flex-col">
-          <div class="relative flex items-center p-3 border-b border-gray-300">
-            <ChatAvatar :UserId="to.id" class="mr-2" />
+          <div class="bg-white px-4 py-5 sm:px-6  border-b border-gray-300">
+            <div class="flex space-x-3">
+              <div class="flex-shrink-0">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </div>
+              <div class="min-w-0 flex-1">
+                <p class="text-sm font-medium text-gray-900">
+                  <a href="#" class="hover:underline">Chelsea Hagon</a>
+                </p>
+                <p class="text-sm text-gray-500">
+                  <a href="#" class="hover:underline">December 9 at 11:43 AM</a>
+                </p>
+              </div>
+              <div class="flex-shrink-0 self-center flex">
+                <Menu as="div" class="relative z-30 inline-block text-left">
+                  <div>
+                    <MenuButton
+                      class="
+                        -m-2
+                        p-2
+                        rounded-full
+                        flex
+                        items-center
+                        text-gray-400
+                        hover:text-gray-600
+                      "
+                    >
+                      <span class="sr-only">Open options</span>
+                      <DotsVerticalIcon class="h-5 w-5" aria-hidden="true" />
+                    </MenuButton>
+                  </div>
 
-            <p class="text-sm font-medium text-gray-900">{{ to.name }}</p>
+                  <transition
+                    enter-active-class="transition ease-out duration-100"
+                    enter-from-class="transform opacity-0 scale-95"
+                    enter-to-class="transform opacity-100 scale-100"
+                    leave-active-class="transition ease-in duration-75"
+                    leave-from-class="transform opacity-100 scale-100"
+                    leave-to-class="transform opacity-0 scale-95"
+                  >
+                    <MenuItems
+                      class="
+                        origin-top-right
+                        absolute
+                        right-0
+                        mt-2
+                        w-56
+                        rounded-md
+                        shadow-lg
+                        bg-white
+                        ring-1 ring-black ring-opacity-5
+                        focus:outline-none
+                      "
+                    >
+                      <div class="py-1">
+                        <MenuItem v-slot="{ active }">
+                          <a
+                            href="#"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'flex px-4 py-2 text-sm',
+                            ]"
+                          >
+                            <StarIcon
+                              class="mr-3 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <span>Add to favorites</span>
+                          </a>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                          <a
+                            href="#"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'flex px-4 py-2 text-sm',
+                            ]"
+                          >
+                            <CodeIcon
+                              class="mr-3 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <span>Embed</span>
+                          </a>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                          <a
+                            href="#"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'flex px-4 py-2 text-sm',
+                            ]"
+                          >
+                            <FlagIcon
+                              class="mr-3 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <span>Report content</span>
+                          </a>
+                        </MenuItem>
+                      </div>
+                    </MenuItems>
+                  </transition>
+                </Menu>
+              </div>
+            </div>
           </div>
           <div
             class="flex flex-col justify-end flex-1 w-full p-6 overflow-y-auto"
@@ -135,6 +245,13 @@
 </template>
 
 <script>
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import {
+  CodeIcon,
+  DotsVerticalIcon,
+  FlagIcon,
+  StarIcon,
+} from "@heroicons/vue/solid";
 import ChatService from "@/services/ChatService.js";
 import { computed, reactive, ref } from "@vue/runtime-core";
 import { useRoute } from "vue-router";
@@ -146,6 +263,14 @@ export default {
   components: {
     PaperAirplaneIcon,
     ChatAvatar,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    CodeIcon,
+    DotsVerticalIcon,
+    FlagIcon,
+    StarIcon,
   },
   setup() {
     let to = ref("");
@@ -160,8 +285,7 @@ export default {
     const authUser = computed(() => store.getters["auth/authUser"]);
 
     const sendMessage = async () => {
-
-       store.dispatch("Chat/getRoomUsers", id).then((result) => {
+      store.dispatch("Chat/getRoomUsers", id).then((result) => {
         let AuthUserId = authUser.value.id;
         if (result.data[0].id == AuthUserId) {
           to.value = result.data[1];
@@ -178,16 +302,16 @@ export default {
             from: from.value.id,
             message: message.value,
             room_id: id,
+            avatar: {
+              img: from.value.avatar,
+              id: from.value.id,
+            },
           },
         };
         socket.emit("message", payload);
-ChatService.sendMessage(payload)
+        ChatService.sendMessage(payload);
         message.value = "";
       }
-
-
-
-
     };
 
     socket.on("message", function (data) {
--- src/components/Chat/RoomList.component.vue
+++ src/components/Chat/RoomList.component.vue
@@ -1,43 +1,121 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div>
-    <ul role="list" class="divide-y divide-gray-200">
-      <li v-for="activityItem in activityItems" :key="activityItem.id" class="py-4">
-        <div class="flex space-x-3">
-          <img class="h-8 w-8 rounded-full" :src="activityItem.person.imageUrl" alt="" />
-          <div class="flex-1 space-y-1">
-            <div class="flex items-center justify-between">
-              <h3 class="text-sm font-medium">{{ activityItem.person.name }}</h3>
-              <p class="text-sm text-gray-500">{{ activityItem.time }}</p>
+  <div class="sm:px-6 lg:px-8 m-10 ">
+    <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Rooms</h3>
+    </div>
+    <div class="bg-white overflow-hidden sm:rounded-md">
+      <div class="px-4 py-4  sm:px-6">
+        <ul role="list" class="divide-y divide-gray-200 w-full">
+          <li
+            v-for="person in people"
+            :key="person.email"
+            class="flex items-center justify-between py-5 "
+          >
+            <div class="flex">
+              <img class="h-10 w-10 rounded-full" :src="person.image" alt="" />
+              <div class="ml-3">
+                <p class="text-sm font-medium text-gray-900">
+                  {{ person.name }}
+                </p>
+                <p class="text-sm text-gray-500">{{ person.email }}</p>
+              </div>
             </div>
-            <p class="text-sm text-gray-500">Deployed {{ activityItem.project }} ({{ activityItem.commit }} in master) to {{ activityItem.environment }}</p>
-          </div>
-        </div>
-      </li>
-    </ul>
+
+            <div class=" sm:mt-0 sm:ml-5">
+              <div
+                class="
+                  flex-shrink-0
+                  sm:w-full
+                  flex-1
+                  h-full
+                  inline-flex
+                  items-center
+                  justify-center
+                  px-4
+                  py-2
+                  rounded-md
+                  text-sm
+                  font-medium
+                  text-black
+                  hover:border-transparent
+                  cursor-pointer
+                "
+                tag="button"
+                @click="CreateRoom"
+              >
+                <PaperAirplaneIcon
+                  class="btn-chat h-5 w-5"
+                  aria-hidden="true"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
+import { PaperAirplaneIcon } from "@heroicons/vue/solid";
+
 const people = [
   {
-    name: 'Lindsay Walton',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80',
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
-  // More people...
-]
-const activityItems = [
-  { id: 1, person: people[0], project: 'Workcation', commit: '2d89f0c8', environment: 'production', time: '1h' },
-  // More items...
-]
+    {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },  {
+    name: "Calvin Hawkins",
+    email: "rah hna??",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
 
 export default {
+  components: {
+    PaperAirplaneIcon,
+  },
   setup() {
     return {
       people,
-      activityItems,
-    }
+    };
   },
-}
+};
 </script>
