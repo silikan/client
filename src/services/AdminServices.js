2ad06e85156afd62b6b9e73fdeb56a1d0b3b27ce
ismailbentabett <ismailbentabett@gmail.com>
2022-04-16 19:15:45 +0100

    client 1.0.0 :  added some admin and moderator logic

--- src/services/AdminServices.js
+++ src/services/AdminServices.js
@@ -1,3 +1,79 @@
 import * as API from "@/services/API";
 
-export default {};
+export default {
+  async deleteUser(id) {
+    return API.delete(`/admin/delete-user/${id}`);
+  },
+  deleteGig(id) {
+    return API.delete(`/admin/delete-gig/${id}`);
+  },
+  deleteClientRequest(id) {
+    return API.delete(`/admin/delete-request/${id}`);
+  },
+  deleteAllUsers() {
+    return API.delete(`/admin/delete-all-users`);
+  },
+  deleteAllGigs() {
+    return API.delete(`/admin/delete-all-gigs`);
+  },
+  deleteAllRequests() {
+    return API.delete(`/admin/delete-all-requests`);
+  },
+  deleteAll() {
+    return API.delete(`/admin/delete-all`);
+  },
+  async getAllUsers() {
+    return API.get(`/admin/get-all-users`);
+  },
+  async getAllGigs() {
+    return API.get(`/admin/get-all-gigs`);
+  },
+  async getAllRequests() {
+    return API.get(`/admin/get-all-requests`);
+  },
+  async getAllHandymen() {
+    return API.get(`/admin/get-all-handymen`);
+  },
+  async getAllClients() {
+    return API.get(`/admin/get-all-clients`);
+  },
+  async getAllAdmins() {
+    return API.get(`/admin/get-all-admins`);
+  },
+  async getAllModerators() {
+    return API.get(`/admin/get-all-moderators`);
+  },
+  makeUserAdmin(id) {
+    return API.put(`/admin/make-user-admin/${id}`);
+  },
+  makeUserModerator(id) {
+    return API.put(`/admin/make-user-moderator/${id}`);
+  },
+  makeUserHandyman(id) {
+    return API.put(`/admin/make-user-handyman/${id}`);
+  },
+  getAllUsersPaginated(page) {
+    return API.get(`/admin/get-all-users-paginated/${page}`);
+  },
+  getAllGigsPaginated(page) {
+    return API.get(`/admin/get-all-gigs-paginated/${page}`);
+  },
+  getAllRequestsPaginated(page) {
+    return API.get(`/admin/get-all-requests-paginated/${page}`);
+  },
+  getAllHandymenPaginated(page) {
+    return API.get(`/admin/get-all-handymen-paginated/${page}`);
+  },
+  getAllClientsPaginated(page) {
+    return API.get(`/admin/get-all-clients-paginated/${page}`);
+  },
+  getAllAdminsPaginated(page) {
+    return API.get(`/admin/get-all-admins-paginated/${page}`);
+  },
+  getAllModeratorsPaginated(page) {
+    return API.get(`/admin/get-all-moderators-paginated/${page}`);
+  },
+  async getLink(link) {
+    return API.apiClient.get(link);
+  },
+};
--- src/store/modules/Admin.js
+++ src/store/modules/Admin.js
@@ -1,9 +1,166 @@
 export const namespaced = true;
 
-export const state = {};
+export const state = {
+  users: [],
+  gigs: [],
+  requests: [],
+  handymen: [],
+  clients: [],
+  admins: [],
+  moderators: [],
+  user: {},
+  gig: {},
+  request: {},
+  handyman: {},
+  client: {},
+  admin: {},
+  moderator: {},
+  usersPaginated: [],
+  gigsPaginated: [],
+  requestsPaginated: [],
+  handymenPaginated: [],
+  clientsPaginated: [],
+  adminsPaginated: [],
+  moderatorsPaginated: [],
+};
 
-export const mutations = {};
+export const mutations = {
+  SET_USERS(state, users) {
+    state.users = users;
+  },
+
+  SET_GIGS(state, gigs) {
+    state.gigs = gigs;
+  },
+
+  SET_REQUESTS(state, requests) {
+    state.requests = requests;
+  },
+
+  SET_HANDYMEN(state, handymen) {
+    state.handymen = handymen;
+  },
+
+  SET_CLIENTS(state, clients) {
+    state.clients = clients;
+  },
+
+  SET_ADMINS(state, admins) {
+    state.admins = admins;
+  },
+
+  SET_MODERATORS(state, moderators) {
+    state.moderators = moderators;
+  },
+  SET_USER(state, user) {
+    state.user = user;
+  },
+  SET_GIG(state, gig) {
+    state.gig = gig;
+  },
+  SET_REQUEST(state, request) {
+    state.request = request;
+  },
+  SET_HANDYMAN(state, handyman) {
+    state.handyman = handyman;
+  },
+  SET_CLIENT(state, client) {
+    state.client = client;
+  },
+  SET_ADMIN(state, admin) {
+    state.admin = admin;
+  },
+  SET_MODERATOR(state, moderator) {
+    state.moderator = moderator;
+  },
+  SET_USERS_PAGINATED(state, usersPaginated) {
+    state.usersPaginated = usersPaginated;
+  },
+  SET_GIGS_PAGINATED(state, gigsPaginated) {
+    state.gigsPaginated = gigsPaginated;
+  },
+  SET_REQUESTS_PAGINATED(state, requestsPaginated) {
+    state.requestsPaginated = requestsPaginated;
+  },
+  SET_HANDYMEN_PAGINATED(state, handymenPaginated) {
+    state.handymenPaginated = handymenPaginated;
+  },
+  SET_CLIENTS_PAGINATED(state, clientsPaginated) {
+    state.clientsPaginated = clientsPaginated;
+  },
+  SET_ADMINS_PAGINATED(state, adminsPaginated) {
+    state.adminsPaginated = adminsPaginated;
+  },
+  SET_MODERATORS_PAGINATED(state, moderatorsPaginated) {
+    state.moderatorsPaginated = moderatorsPaginated;
+  },
+};
 
 export const actions = {};
 
-export const getters = {};
+export const getters = {
+  users(state) {
+    return state.users;
+  },
+  gigs(state) {
+    return state.gigs;
+  },
+  requests(state) {
+    return state.requests;
+  },
+  handymen(state) {
+    return state.handymen;
+  },
+  clients(state) {
+    return state.clients;
+  },
+  admins(state) {
+    return state.admins;
+  },
+
+  moderators(state) {
+    return state.moderators;
+  },
+  user(state) {
+    return state.user;
+  },
+  gig(state) {
+    return state.gig;
+  },
+  request(state) {
+    return state.request;
+  },
+  handyman(state) {
+    return state.handyman;
+  },
+  client(state) {
+    return state.client;
+  },
+  admin(state) {
+    return state.admin;
+  },
+  moderator(state) {
+    return state.moderator;
+  },
+  usersPaginated(state) {
+    return state.usersPaginated;
+  },
+  gigsPaginated(state) {
+    return state.gigsPaginated;
+  },
+  requestsPaginated(state) {
+    return state.requestsPaginated;
+  },
+  handymenPaginated(state) {
+    return state.handymenPaginated;
+  },
+  clientsPaginated(state) {
+    return state.clientsPaginated;
+  },
+  adminsPaginated(state) {
+    return state.adminsPaginated;
+  },
+  moderatorsPaginated(state) {
+    return state.moderatorsPaginated;
+  },
+};
