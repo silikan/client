ce2e73f5d3b8a22167781cc76aeae15a38df89bf
ismailbentabett <ismailbentabett@gmail.com>
2022-04-23 19:25:40 +0100

    client 1.0.0 : create gig generate form validation

--- src/components/Gig/CreateGig/General.component.vue
+++ src/components/Gig/CreateGig/General.component.vue
@@ -12,7 +12,7 @@
           <p class="mt-1 max-w-2xl text-sm text-gray-500">create that gig</p>
         </div>
         <div class="space-y-6 sm:space-y-5">
-          <div class="-gray-300 rounded-lg shadow-sm overflow-hidden">
+          <div class="-gray-300 rounded-lg -sm overflow-hidden">
             <label
               for="title"
               class="block text-sm font-medium text-black sm:mt-px sm:pt-2"
@@ -20,34 +20,47 @@
               Title
             </label>
 
-            <div class="mt-1 sm:mt-0 sm:col-span-2">
+            <div class="mt-1 relative rounded-md -sm">
               <input
                 v-model="title"
                 type="text"
                 name="title"
                 id="title"
                 autocomplete="on"
-                class="
-                  appearance-none
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-gray-300
-                  rounded-md
-                  shadow-sm
-                  placeholder-gray-400
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
-                  sm:text-sm
-                "
+                :class="[
+                  titleErrorMessage
+                    ? 'block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md w-full px-3 py-2 border border-red-300  text-red-900 rounded-md  -smplaceholder-red-400 focus:outline-nonefocus:ring-red-500 focus:border-red-500 sm:text-sm'
+                    : ' block w-full px-3 py-2 border border-gray-300 rounded-md  -smplaceholder-gray-400 focus:outline-nonefocus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                ]"
               />
+              <div
+                class="
+                  absolute
+                  inset-y-0
+                  right-0
+                  pr-3
+                  flex
+                  items-center
+                  pointer-events-none
+                "
+              >
+                <ExclamationCircleIcon
+                  class="h-5 w-5 text-red-500"
+                  aria-hidden="true"
+                  v-if="titleErrorMessage"
+                />
+              </div>
             </div>
-            <span>{{ errorMessage }}</span>
+            <p
+              v-if="titleErrorMessage"
+              class="mt-2 text-sm text-red-600"
+              id="email-error"
+            >
+              {{ titleErrorMessage }}
+            </p>
           </div>
 
-          <div class="-gray-300 rounded-lg shadow-sm overflow-hidden">
+          <div class="-gray-300 rounded-lg -sm overflow-hidden">
             <label
               for="category"
               class="block text-sm font-medium text-black sm:mt-px sm:pt-2"
@@ -60,27 +73,41 @@
                 id="category"
                 name="category"
                 autocomplete="on"
-                class="
-                  appearance-none
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-gray-300
-                  rounded-md
-                  shadow-sm
-                  placeholder-gray-400
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
-                  sm:text-sm
-                "
+                :class="[
+                  categoryErrorMessage
+                    ? 'block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md w-full px-3 py-2 border border-red-300  text-red-900 rounded-md  -smplaceholder-red-400 focus:outline-nonefocus:ring-red-500 focus:border-red-500 sm:text-sm'
+                    : ' block w-full px-3 py-2 border border-gray-300 rounded-md  -smplaceholder-gray-400 focus:outline-nonefocus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                ]"
               >
                 <option v-for="item in categories" :key="item" :value="item">
                   {{ item }}
                 </option>
               </select>
+            <div
+                class="
+                  absolute
+                  inset-y-0
+                  right-0
+                  pr-3
+                  flex
+                  items-center
+                  pointer-events-none
+                "
+              >
+                <ExclamationCircleIcon
+                  class="h-5 w-5 text-red-500"
+                  aria-hidden="true"
+                  v-if="categoryErrorMessage"
+                />
+              </div>
             </div>
+            <p
+              v-if="categoryErrorMessage"
+              class="mt-2 text-sm text-red-600"
+              id="email-error"
+            >
+              {{ categoryErrorMessage }}
+            </p>
           </div>
         </div>
       </div>
@@ -88,7 +115,7 @@
       <div class="flex items-start space-x-4 pb-">
         <div class="min-w-0 flex-1">
           <div action="#" class="relative">
-            <div class="-gray-300 rounded-lg shadow-sm overflow-hidden">
+            <div class="-gray-300 rounded-lg -sm overflow-hidden">
               <label
                 for="description"
                 class="block text-sm font-medium text-black sm:mt-px sm:pt-2"
@@ -101,63 +128,38 @@
                 rows="3"
                 name="description"
                 id="description"
-                class="
-                  block
-                  w-full
-                  py-3
-                  h-40
-                  -0
-                  resize-none
-                  focus:ring-0
-                  sm:text-sm
-                  appearance-none
-                  px-3
-                  border border-gray-300
-                  rounded-md
-                  shadow-sm
-                  placeholder-gray-400
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
+                :class="[descriptionErrorMessage ?'block w-full py-3 h-40 -0 resize-none focus:ring-0  sm:text-sm  appearance-nonepx-3 text-red-900 border border-red-300 rounded-md-sm placeholder-red-400 focus:outline-none focus:ring-red-500 focus:border-red-500' : 'block w-full py-3 h-40 -0 resize-nonefocus:ring-0  sm:text-sm  appearance-nonepx-3 border border-gray-300rounded-md-smplaceholder-gray-400 focus:outline-nonefocus:ring-indigo-500 focus:border-indigo-500']
                 "
                 placeholder="Add your description..."
               />
 
-              <!-- Spacer element to match the height of the toolbar -->
-              <div class="py-2" aria-hidden="true">
-                <!-- Matches height of button in toolbar (1px  + 36px content height) -->
-                <div class="py-px">
-                  <div class="h-9" />
-                </div>
+           <div
+                class="
+                  absolute
+                  inset-y-0
+                  right-0
+                  pr-3
+                  flex
+                  items-center
+                  pointer-events-none
+                "
+              >
+                <ExclamationCircleIcon
+                  class="h-5 w-5 text-red-500"
+                  aria-hidden="true"
+                  v-if="descriptionErrorMessage"
+                />
               </div>
             </div>
-
-            <div
-              class="absolute bottom-0 inset-x-0 pl-3 py-2 flex justify-between"
+            <p
+              v-if="descriptionErrorMessage"
+              class="mt-2 text-sm text-red-600"
+              id="email-error"
             >
-              <div class="flex items-center space-x-5">
-                <div class="flex items-center">
-                  <button
-                    type="button"
-                    class="
-                      -m-2.5
-                      w-10
-                      h-10
-                      rounded-full
-                      flex
-                      items-center
-                      justify-center
-                      text-gray-400
-                      hover:text-gray-500
-                    "
-                  >
-                    <PaperClipIcon class="h-5 w-5" aria-hidden="true" />
-                    <span class="sr-only">Attach a file</span>
-                  </button>
-                </div>
-              </div>
-              <div class="flex-shrink-0"></div>
-            </div>
+              {{ descriptionErrorMessage }}
+            </p>
+
+
           </div>
         </div>
       </div>
@@ -165,10 +167,11 @@
   </form>
 </template>
 <script>
-import { PaperClipIcon } from "@heroicons/vue/solid";
 import { useStore } from "vuex";
 import { watchEffect } from "@vue/runtime-core";
 import { useField } from "vee-validate";
+import { ExclamationCircleIcon } from "@heroicons/vue/solid";
+
 import * as yup from "yup";
 
 let categories = [
@@ -189,7 +192,7 @@ let categories = [
 
 export default {
   components: {
-    PaperClipIcon,
+    ExclamationCircleIcon,
   },
   setup() {
     let store = useStore();
