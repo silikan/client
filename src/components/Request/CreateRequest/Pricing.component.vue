482153c946cc712dde8dfe24e7e6bb03bb1e810a
ismailbentabett <ismailbentabett@gmail.com>
2022-04-23 21:07:39 +0100

    client 1.0.0 : completed request validation for now

--- src/components/Request/CreateRequest/Pricing.component.vue
+++ src/components/Request/CreateRequest/Pricing.component.vue
@@ -27,33 +27,15 @@
                 <span class="text-gray-500 sm:text-sm"> $ </span>
               </div>
               <input
-              v-model="price"
                 type="number"
                 name="price"
                 id="price"
-                class="
-                  focus:ring-indigo-500 focus:border-indigo-500
-                  block
-                  w-full
-                  pl-7
-                  pr-12
-                  sm:text-sm
-                  border-gray-300
-                  rounded-md
-                  appearance-none
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-gray-300
-                  rounded-md
-                  shadow-sm
-                  placeholder-gray-400
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
-                  sm:text-sm
-                "
+                v-model="price"
+                :class="[
+                  priceErrorMessage
+                    ? 'block w-full  pr-12  pl-7 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md w-full px-3 py-2 border border-red-300  text-red-900 rounded-md  -smplaceholder-red-400 focus:outline-nonefocus:ring-red-500 focus:border-red-500 sm:text-sm'
+                    : ' block w-full pr-12  pl-7 px-3 py-2 border border-gray-300 rounded-md  -smplaceholder-gray-400 focus:outline-nonefocus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                ]"
                 placeholder="0.00"
                 aria-describedby="price-currency"
               />
@@ -72,49 +54,66 @@
                   DZD
                 </span>
               </div>
+
+              <div
+                class="
+                  absolute
+                  inset-y-0
+                  right-0
+                  flex
+                  items-center
+                  pointer-events-none
+                "
+              >
+                <ExclamationCircleIcon
+                  class="h-5 w-5 text-red-500"
+                  aria-hidden="true"
+                  v-if="priceErrorMessage"
+                />
+              </div>
             </div>
+            <p
+              v-if="priceErrorMessage"
+              class="mt-2 text-sm text-red-600"
+              id="email-error"
+            >
+              {{ priceErrorMessage }}
+            </p>
           </div>
         </div>
       </div>
-    <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-
+      <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
         <div class="space-y-6 sm:space-y-5">
           <div class="-gray-300 rounded-lg shadow-sm overflow-hidden">
             <label for="price" class="block text-sm font-medium text-gray-700"
               >Duration</label
             >
             <div class="mt-1 relative rounded-md shadow-sm">
-
+              <div
+                class="
+                  absolute
+                  inset-y-0
+                  left-0
+                  pl-3
+                  flex
+                  items-center
+                  pointer-events-none
+                "
+              >
+                <span class="text-gray-500 sm:text-sm"> $ </span>
+              </div>
               <input
-              v-model="duration"
                 type="number"
                 name="duration"
                 id="duration"
-                class="
-                  focus:ring-indigo-500 focus:border-indigo-500
-                  block
-                  w-full
-                  pl-7
-                  pr-12
-                  sm:text-sm
-                  border-gray-300
-                  rounded-md
-                  appearance-none
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-gray-300
-                  rounded-md
-                  shadow-sm
-                  placeholder-gray-400
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
-                  sm:text-sm
-                "
-                placeholder="0"
-                aria-describedby="price-currency"
+                v-model="duration"
+                :class="[
+                  durationErrorMessage
+                    ? 'block w-full  pr-12  pl-7 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md w-full px-3 py-2 border border-red-300  text-red-900 rounded-md  -smplaceholder-red-400 focus:outline-nonefocus:ring-red-500 focus:border-red-500 sm:text-sm'
+                    : ' block w-full pr-12  pl-7 px-3 py-2 border border-gray-300 rounded-md  -smplaceholder-gray-400 focus:outline-nonefocus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                ]"
+                placeholder="0.00"
+                aria-describedby="duration"
               />
               <div
                 class="
@@ -131,7 +130,31 @@
                   Days
                 </span>
               </div>
+
+              <div
+                class="
+                  absolute
+                  inset-y-0
+                  right-0
+                  flex
+                  items-center
+                  pointer-events-none
+                "
+              >
+                <ExclamationCircleIcon
+                  class="h-5 w-5 text-red-500"
+                  aria-hidden="true"
+                  v-if="durationErrorMessage"
+                />
+              </div>
             </div>
+            <p
+              v-if="durationErrorMessage"
+              class="mt-2 text-sm text-red-600"
+              id="email-error"
+            >
+              {{ durationErrorMessage }}
+            </p>
           </div>
         </div>
       </div>
@@ -146,7 +169,7 @@
                 Description
               </label>
               <textarea
-              v-model="description"
+                v-model="description"
                 rows="3"
                 name="description"
                 id="description"
@@ -191,27 +214,53 @@
   </form>
 </template>
 <script>
-import { ref } from "@vue/reactivity";
 import { watchEffect } from "@vue/runtime-core";
-import { useStore } from 'vuex';
+import { useStore } from "vuex";
+import { useField } from "vee-validate";
+import { ExclamationCircleIcon } from "@heroicons/vue/solid";
+import * as yup from "yup";
 
 export default {
-  components: {},
+  components: { ExclamationCircleIcon },
   setup() {
-    let price = ref(0);
-    let description = ref("");
-    let duration = ref(0);
-let store = useStore();
-    watchEffect(() => {
-        store.commit("Request/SET_PRICE", price.value);
-        store.commit("Request/SET_PRICE_DESCRIPTION", description.value);
-        store.commit("Request/SET_DURATION", duration.value);
+    let store = useStore();
 
+    let priceValidation = yup
+      .number()
+      .min(0.01, "Price must be greater than 0")
+      .required("Price is required");
+    let durationValidation = yup
+      .number()
+      .min(0.01, "Duration must be greater than 0")
+      .required("Duration is required");
+    let descriptionValidation = yup
+      .string()
+      .required("Description is required")
+      .min(3, "Description must be at least 3 characters long")
+      .max(500, "Description must be less than 500 characters long");
+    const { value: duration, errorMessage: durationErrorMessage } = useField(
+      "duration",
+      durationValidation
+    );
+    const { value: price, errorMessage: priceErrorMessage } = useField(
+      "price",
+      priceValidation
+    );
+    const { value: description, errorMessage: descriptionErrorMessage } =
+      useField("description", descriptionValidation);
+
+    watchEffect(() => {
+      store.commit("Request/SET_PRICE", price.value);
+      store.commit("Request/SET_PRICE_DESCRIPTION", description.value);
+      store.commit("Request/SET_DURATION", duration.value);
     });
     return {
       price,
       description,
-      duration
+      duration,
+      priceErrorMessage,
+      descriptionErrorMessage,
+      durationErrorMessage,
     };
   },
 };
