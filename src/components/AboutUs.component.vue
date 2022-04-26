6e9c3856e6539bb95578413189213d287a5abe99
Kamel Cerbah <ren.cerbah@gmail.com>
2022-04-26 19:11:53 +0100

    client 1.0.0 : minor changes

--- src/components/AboutUs.component.vue
+++ src/components/AboutUs.component.vue
@@ -17,7 +17,7 @@
         <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
           <!-- Testimonial card-->
           <div class="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
-            <img class="absolute inset-0 h-full w-full object-cover" src="../assets/Testemonials/kamel.jpg" alt="" />
+            <img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100" alt="" />
             <div class="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
             <div class="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
             <div class="relative px-8">
@@ -35,7 +35,7 @@
                 </div>
 
                 <footer class="mt-4">
-                  <p class="text-base font-semibold text-indigo-200">Kamel Cerbah, CTO at silikan</p>
+                  <p class="text-base font-semibold text-indigo-200">Sarah Williams, CTO at silikan</p>
                 </footer>
               </blockquote>
             </div>
