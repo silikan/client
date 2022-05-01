d9042172dfead3366a3785af24b8f57342315380
ismailbentabett <ismailbentabett@gmail.com>
2022-05-01 05:47:00 +0100

    client 1.0.0 : yaaaaaaaaay progress

--- src/components/FeedBack/FloatingActionButton.component.vue
+++ src/components/FeedBack/FloatingActionButton.component.vue
@@ -1,50 +1,133 @@
 <template>
-<div>
-  <div
+  <div>
+    <div
+      aria-live="assertive"
+      class="fixed inset-0 flex px-4 py-6 pointer-events-none sm:p-6 items-end"
+    >
+      <div class="w-full flex flex-col items-end space-y-4 sm:items-end">
+        <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
 
-    aria-live="assertive"
-    class="fixed inset-0 flex px-4 py-6 pointer-events-none sm:p-6 items-end"
-  >
-    <div class="w-full flex flex-col items-end space-y-4 sm:items-end">
-      <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
+        <div class="pointer-events-auto">
+          <Popper placement="top">
+            <button
+              type="button"
+              class="
+                inline-flex
+                items-center
+                p-3
+                border border-transparent
+                rounded-full
+                shadow-sm
+                text-white
+                bg-indigo-600
+                hover:bg-indigo-700
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                focus:ring-indigo-500
+              "
+            >
+              <!-- Heroicon name: outline/plus-sm -->
+              <QuestionMarkCircleIcon
+                class="h-5 w-5 text-white"
+                aria-hidden="true"
+              />
+            </button>
 
-        <div
-          class="
-
-            pointer-events-auto
-
-          "
-        >
-  <button type="button" class="inline-flex items-center p-3 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-  <!-- Heroicon name: outline/plus-sm -->
-<QuestionMarkCircleIcon
-                              class=" h-5 w-5 text-white"
-                              aria-hidden="true"
-                            />
-</button>
-
-
-
-
-  </div>
+            <template #content>
+				<div class="flex flex-col space-y-3">
+              <button
+                type="button"
+                class="
+                  inline-flex
+                  items-center
+                  p-3
+                  border border-transparent
+                  rounded-full
+                  shadow-sm
+                  text-white
+                  bg-indigo-600
+                  hover:bg-indigo-700
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-offset-2
+                  focus:ring-indigo-500
+                "
+              >
+                <!-- Heroicon name: outline/plus-sm -->
+                <QuestionMarkCircleIcon
+                  class="h-5 w-5 text-white"
+                  aria-hidden="true"
+                />
+              </button>
+			     <button
+                type="button"
+                class="
+                  inline-flex
+                  items-center
+                  p-3
+                  border border-transparent
+                  rounded-full
+                  shadow-sm
+                  text-white
+                  bg-indigo-600
+                  hover:bg-indigo-700
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-offset-2
+                  focus:ring-indigo-500
+                "
+              >
+                <!-- Heroicon name: outline/plus-sm -->
+                <QuestionMarkCircleIcon
+                  class="h-5 w-5 text-white"
+                  aria-hidden="true"
+                />
+              </button>
+			     <button
+                type="button"
+                class="
+                  inline-flex
+                  items-center
+                  p-3
+                  border border-transparent
+                  rounded-full
+                  shadow-sm
+                  text-white
+                  bg-indigo-600
+                  hover:bg-indigo-700
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-offset-2
+                  focus:ring-indigo-500
+                "
+              >
+                <!-- Heroicon name: outline/plus-sm -->
+                <QuestionMarkCircleIcon
+                  class="h-5 w-5 text-white"
+                  aria-hidden="true"
+                />
+              </button>
+			  </div>
+            </template>
+          </Popper>
+        </div>
+      </div>
     </div>
-
   </div>
-    </div>
-
 </template>
 
 <script>
-import {
- QuestionMarkCircleIcon
-} from "@heroicons/vue/solid";
+import Popper from "vue3-popper";
+
+import { QuestionMarkCircleIcon } from "@heroicons/vue/solid";
 export default {
-components: {
-	QuestionMarkCircleIcon
-},
-}
+  components: {
+    QuestionMarkCircleIcon,
+    Popper,
+  },
+};
 </script>
 
 <style>
-
 </style>
