b779c4c0dadd46d2f5ce8bd95863dc9885ba2a41
ismailbentabett <ismailbentabett@gmail.com>
2022-05-18 01:39:50 +0100

    client 1.0.0 : added dark mode to signin

--- src/views/Signin.view.vue
+++ src/views/Signin.view.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-  <Signin />
+  <div class="dark:bg-prblue-500">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <Signin />
+    </div>
   </div>
 </template>
 
 <script>
-import Signin from '../components/Auth/Signin.component.vue';
+import Signin from "../components/Auth/Signin.component.vue";
 
 export default {
   components: { Signin },
