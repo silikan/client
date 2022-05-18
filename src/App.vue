be191f20bfba1a3295a116525a2fc1e7afd6230f
ismailbentabett <ismailbentabett@gmail.com>
2022-05-18 01:14:35 +0100

    client 1.0.0 : added dark mode to home

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="h-screen min-h-full flex flex-col justify-between">
+  <div
+    class="
+      h-screen
+      min-h-full
+      flex flex-col
+      justify-between
+    "
+  >
     <Teleport to=".notifications">
       <BasicNotification class="z-50 mt-10" v-if="isLoggedin" />
     </Teleport>
-<FeedbackModalComponent/>
+    <FeedbackModalComponent />
     <Teleport to=".action">
       <FloatingActionButton />
     </Teleport>
@@ -33,7 +40,7 @@ export default {
     LoadingBar,
     BasicNotification,
     FloatingActionButton,
-    FeedbackModalComponent
+    FeedbackModalComponent,
   },
 
   setup() {
